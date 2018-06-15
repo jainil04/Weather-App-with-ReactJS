d43d2bca6a0c73bfbd2a9c26a4cf86a332833ff8
jainil04 <jainil04@gmail.com>
2018-06-15 01:28:11 -0700

    first commit

---
+++ src/component/Title.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Title = () => (
+    <div>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <p className="title-container__subtitle">Find Weather</p>
+    </div>
+);
+export default Title;
